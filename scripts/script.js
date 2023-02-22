ef25d798b7c626f8cacd48e7f6fd05d66a169fa8
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-02-22 10:43:29 +0100

    Added Pagination

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,5 @@
-function showFirst() {
-    const main = document.getElementById("main");
-    //main.addEventListener("load", showAllCharacters())
-}
-
-function showAllCharacters() {
-    fetch("https://rickandmortyapi.com/api/character")
+function showCharacters() {
+    fetch("https://rickandmortyapi.com/api/character?name=" + currentName + "&page=" + currentPage)
         .then(function (response) {
             return response.json();
         })
@@ -13,15 +8,26 @@ function showAllCharacters() {
             for(let i = 0; i < json.results.length; i++) {
                 document.getElementById("characters").innerHTML += getCharacterCard(json.results[i]);
             }
+            currentPage++
         });
 }
 
 function getCharacterCard(character) {
     let img = "<img class='card-image' src='" + character.image + "'/>"
     let name = "<p class='card-name'>" + character.name + "</p>"
-    let description = "<p class='card-text'>" + character.gender + "</p>"
+    let description = "<p class='card-text'>Gender: " + character.gender + "<br/>"
+        + "Status: " + character.status + "</br>"
+        + "</p>"
     return "<div class='main-card'>" + img + name + description + "</div>"
 }
 
-showFirst();
-showAllCharacters();
+function searchName() {
+    currentName = document.getElementById("searchBar").value;
+    currentPage = 1;
+    document.getElementById("characters").innerHTML = "";
+    showCharacters();
+}
+
+let currentPage = 1;
+let currentName = "";
+showCharacters("");
